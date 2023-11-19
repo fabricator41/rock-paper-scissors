687efbb8280c1b4c23223da0ff58dd0949357257
Emre İlhan <emrilhan@gmail.com>
2023-11-19 21:13:28 +0300

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = "";
+let computerSelection = "";
+
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector("#paper");
+const scissorsbtn = document.querySelector("#scissors");
+const result = document.querySelector("#result");
+
+rockbtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playerSelection = rockbtn.id;
+  result.textContent = `Player Score: ${playerScore}
+  Computer Score: ${computerScore}`;
+  result.textContent += " " + playRound(playerSelection, computerSelection);
+});
+paperbtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playerSelection = paperbtn.id;
+  result.textContent = playRound(playerSelection, computerSelection);
+});
+scissorsbtn.addEventListener("click", () => {
+  computerSelection = getComputerChoice();
+  playerSelection = scissorsbtn.id;
+  result.textContent = playRound(playerSelection, computerSelection);
+});
+
 function getComputerChoice() {
   let computerChoice = "";
   let randomChoice = Math.random();
@@ -11,76 +39,26 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerSelection, computerSelection) {
-  let lowerCaseComputerSelection = computerSelection.toLowerCase();
-
-  if (playerSelection === lowerCaseComputerSelection) {
+  if (playerSelection === computerSelection) {
     return "It's a tie!";
-  } else if (
-    playerSelection === "rock" &&
-    lowerCaseComputerSelection === "scissors"
-  ) {
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
     return "Player Wins! Rock beats Scissors";
-  } else if (
-    playerSelection === "scissors" &&
-    lowerCaseComputerSelection === "rock"
-  ) {
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
     return "Computer Wins! Rock beats Scissors";
-  } else if (
-    playerSelection === "paper" &&
-    lowerCaseComputerSelection === "rock"
-  ) {
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
     return "Player Wins! Paper beats Rock";
-  } else if (
-    playerSelection === "rock" &&
-    lowerCaseComputerSelection === "paper"
-  ) {
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
     return "Computer Wins! Paper beats Rock";
-  } else if (
-    playerSelection === "scissors" &&
-    lowerCaseComputerSelection === "paper"
-  ) {
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
     return "Player Wins! Scissors beats Paper";
-  } else if (
-    playerSelection === "paper" &&
-    lowerCaseComputerSelection === "scissors"
-  ) {
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
     return "Computer Wins! Paper beats Rock";
   }
 }
-
-function game() {
-  let playerSelection = prompt("Rock, Paper, Scissors?");
-  let computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  playerSelection = prompt("Rock, Paper, Scissors?");
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  playerSelection = prompt("Rock, Paper, Scissors?");
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  playerSelection = prompt("Rock, Paper, Scissors?");
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  playerSelection = prompt("Rock, Paper, Scissors?");
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  if (playerScore < computerScore) {
-    console.log("Computer Wins the Game!");
-  } else if (playerScore > computerScore) {
-    console.log("Player wins the Game!");
-  } else {
-    console.log("The game is a Tie!");
-  }
-}
-
-game();
